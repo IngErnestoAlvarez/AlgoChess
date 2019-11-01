d9cf99afaa245a31c62cb36d452fad8a67f5a4f4
Alex <alejocalizblanco@gmail.com>
2019-11-01 14:16:54 -0300

    Inicializado el tablero con celdas falsas

---
+++ src/main/java/Celda/Celda.java
@@ -0,0 +1,6 @@
+package Celda;
+
+public class Celda {
+    public Celda(int posicionHorizontal, int posicionVertical) {
+    }
+}
--- src/main/java/Tablero/Tablero.java
+++ src/main/java/Tablero/Tablero.java
@@ -1,8 +1,22 @@
 package Tablero;
 
+import Celda.Celda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Tablero {
 
+    private List <Celda> celdas;
+
     public Tablero(int largo, int alto) {
+        celdas = new ArrayList<Celda>();
+        for (int i = 0; i < largo; i++ ){
+            for ( int j = 0; j < alto; j++ ){
+                celdas.add(new Celda(i,j));
+            }
+        }
 
     }
 }
