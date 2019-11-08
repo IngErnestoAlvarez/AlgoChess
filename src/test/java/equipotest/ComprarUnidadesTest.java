377dc04028f3ea64293a68943dba407cd96a5ca8
Daniela V <56934023+noxethiems@users.noreply.github.com>
2019-11-08 17:59:49 -0300

    Test cambiados para estar acorde a patron de diseño

--- src/test/java/equipotest/ComprarUnidadesTest.java
+++ src/test/java/equipotest/ComprarUnidadesTest.java
@@ -20,89 +20,91 @@ public class ComprarUnidadesTest extends TestCase{
         assertEquals (20, equipo.getPuntaje());
     }
 
-    public void test02EquipoCompraUnSoldado() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades {
+    public void test02EquipoCompraUnSoldado() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades, EquipoQuiereCrearUnidadInvalida {
 
         Equipo equipo = new Equipo();
-        equipo.comprarSoldado();
+        equipo.comprarUnidad("soldado");
 
         assertTrue (equipo.tieneUnidades()); //Ver
         assertEquals (1, equipo.getCantidadUnidades());
         assertEquals ( 19, equipo.getPuntaje());
     }
 
-    public void test03EquipoCompraUnaCatapulta() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades {
+    public void test03EquipoCompraUnaCatapulta() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades, EquipoQuiereCrearUnidadInvalida {
 
         Equipo equipo = new Equipo();
-        equipo.comprarCatapulta();
+        equipo.comprarUnidad("CATAPulta");
 
         assertTrue (equipo.tieneUnidades()); //Ver
         assertEquals (1, equipo.getCantidadUnidades());
         assertEquals ( 15, equipo.getPuntaje());
     }
 
-    public void test04EquipoCompraUnJinete() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades {
+    public void test04EquipoCompraUnJinete() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades, EquipoQuiereCrearUnidadInvalida {
 
         Equipo equipo = new Equipo();
-        equipo.comprarJinete();
+        equipo.comprarUnidad("JiNeTe");
 
         assertTrue (equipo.tieneUnidades());
         assertEquals (1, equipo.getCantidadUnidades());
         assertEquals ( 17, equipo.getPuntaje());
     }
 
-    public void test05EquipoCompraUnCurandero() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades {
+    public void test05EquipoCompraUnCurandero() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades, EquipoQuiereCrearUnidadInvalida {
 
         Equipo equipo = new Equipo();
-        equipo.comprarCurandero();
+        equipo.comprarUnidad("curandero");
 
         assertTrue (equipo.tieneUnidades());
         assertEquals (1, equipo.getCantidadUnidades());
         assertEquals ( 18, equipo.getPuntaje());
     }
 
-    public void test06EquipoGastaTodosSusPuntos() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades {
+    public void test06EquipoGastaTodosSusPuntos() throws EquipoNoTienePuntosSuficientes, EquipoNoPuedeComprarMasUnidades, EquipoQuiereCrearUnidadInvalida {
 
         Equipo equipo = new Equipo();
 
-        equipo.comprarCatapulta();
-        equipo.comprarCatapulta();
-        equipo.comprarCatapulta();
-        equipo.comprarCatapulta();
+        equipo.comprarUnidad("catapulta");
+        equipo.comprarUnidad("catapulta");
+        equipo.comprarUnidad("catapulta");
+        equipo.comprarUnidad("catapulta");
 
         assertTrue(equipo.tieneUnidades()); //Ver
         assertEquals(4, equipo.getCantidadUnidades());
         assertEquals(0, equipo.getPuntaje());
     }
 
-    public void test07ExcepcionEquipoIntentaExcederGastos() throws EquipoNoTienePuntosSuficientes {
+    public void test07ExcepcionEquipoIntentaExcederGastos() throws EquipoNoTienePuntosSuficientes, EquipoQuiereCrearUnidadInvalida {
 
         Equipo equipo = new Equipo();
 
         try {
-            equipo.comprarCatapulta();
-            equipo.comprarCatapulta();
-            equipo.comprarCatapulta();
-            equipo.comprarCatapulta();
+            equipo.comprarUnidad("catapulta");
+            equipo.comprarUnidad("catapulta");
+            equipo.comprarUnidad("catapulta");
+            equipo.comprarUnidad("catapulta");
 
-            equipo.comprarSoldado();
+            equipo.comprarUnidad("soldado");
+
             fail( "No se lanzo la excepcion." );
 
         }catch (EquipoNoPuedeComprarMasUnidades excepcion ) {
         }
     }
 
-    public void test08ExcepcionEquipoNoTienePuntosSuficientes() throws EquipoNoTienePuntosSuficientes{
+    public void test08ExcepcionEquipoNoTienePuntosSuficientes() throws EquipoNoTienePuntosSuficientes, EquipoQuiereCrearUnidadInvalida{
 
         Equipo equipo = new Equipo();
         try {
 
-            equipo.comprarCatapulta();
-            equipo.comprarCatapulta();
-            equipo.comprarCatapulta();
-            equipo.comprarCatapulta();
+            equipo.comprarUnidad("catapulta");
+            equipo.comprarUnidad("catapulta");
+            equipo.comprarUnidad("catapulta");
+            equipo.comprarUnidad("catapulta");
 
-            equipo.comprarJinete();
+            equipo.comprarUnidad("jinete");
             fail( "No se lanzo la excepcion." );
+
         }catch (EquipoNoPuedeComprarMasUnidades excepcion ) {
         }
     }
