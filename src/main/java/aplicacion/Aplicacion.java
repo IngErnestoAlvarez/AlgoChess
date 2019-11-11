072e2d4bc113a44a3755ebe920fe1981d4f6c2ab
Ernesto Alvarez <ernesto.alvarez1812@gmail.com>
2019-11-11 09:22:42 -0300

    Se creó la aplicación para javaFX

---
+++ src/main/java/aplicacion/Aplicacion.java
@@ -0,0 +1,16 @@
+package aplicacion;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Aplicacion extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
