8ea63908db42b2e06cb655776cd19f910018990e
Daniela V <dani_ailin14@outlook.com>
2019-11-14 17:57:47 -0300

    Vuelta a la original (hash)

--- src/main/java/tablero/movimientos/CalculadorDeMovimientos.java
+++ src/main/java/tablero/movimientos/CalculadorDeMovimientos.java
@@ -7,27 +7,20 @@ import java.util.HashMap;
 public class CalculadorDeMovimientos {
 
     private HashMap fabricaMovimientos;
-    private HashMap hashDeStrings;
 
     public CalculadorDeMovimientos(Posicion posicionOrigen) {
 
         this.fabricaMovimientos = new HashMap(4,2);
-        this.hashDeStrings = new HashMap(4,2);
 
-        hashDeStrings.put(posicionOrigen.arriba(),"Arriba");
-        hashDeStrings.put(posicionOrigen.abajo(),"Abajo");
-        hashDeStrings.put(posicionOrigen.izquierda(),"Izquierda");
-        hashDeStrings.put(posicionOrigen.derecha(),"Derecha");
-
-        this.fabricaMovimientos.put(hashDeStrings.get(posicionOrigen.izquierda()), new Izquierda());
-        this.fabricaMovimientos.put(hashDeStrings.get(posicionOrigen.derecha()), new Derecha());
-        this.fabricaMovimientos.put(hashDeStrings.get(posicionOrigen.arriba()) , new Arriba());
-        this.fabricaMovimientos.put(hashDeStrings.get(posicionOrigen.abajo()), new Abajo());
+        this.fabricaMovimientos.put(posicionOrigen.izquierda(), new Izquierda());
+        this.fabricaMovimientos.put(posicionOrigen.derecha(), new Derecha());
+        this.fabricaMovimientos.put(posicionOrigen.arriba() , new Arriba());
+        this.fabricaMovimientos.put(posicionOrigen.abajo(), new Abajo());
 
     }
 
     public Movimiento crearMovimiento(Posicion posicionDestino) {
-        Movimiento miMovimiento = (Movimiento) this.fabricaMovimientos.get(hashDeStrings.get(posicionDestino));
+        Movimiento miMovimiento = (Movimiento) this.fabricaMovimientos.get(posicionDestino);
         return miMovimiento;
     }
 
